74e3a88aa3fe4aa149ae9f32cbf14542aed2f65c
narendra25 <anrsmile96@gmail.com>
2023-07-14 14:31:25 +0530

    Added Login Page Objects

--- OrangeHRM_Testing/src/PageObjects/LoginPageObjects.java
+++ OrangeHRM_Testing/src/PageObjects/LoginPageObjects.java
@@ -1,5 +1,26 @@
 package PageObjects;
 
-public class LoginPageObjects {
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 
+import TestBase.TestBase;
+
+public class LoginPageObjects extends TestBase{
+	 //Enter The User Name 
+		public static void Enter_The_UserName() {
+			WebElement UserName=driver.findElement(By.xpath(prop.getProperty("UserName")));
+			UserName.sendKeys(prop.getProperty("username"));
+		}
+		
+		//Enter The Password
+		public static void Enter_The_Password() {
+			WebElement Password=driver.findElement(By.xpath(prop.getProperty("Password")));
+			Password.sendKeys(prop.getProperty("password"));
+		}
+		
+		//Click On Login Button
+		public static void Click_On_Login_Button() {
+			WebElement LoginButton=driver.findElement(By.xpath(prop.getProperty("LoginButton")));
+			LoginButton.click();
+		}
 }
